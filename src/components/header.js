32f7b46e399282428664584d3af3b7357bab3c2c
Skillz <gankstarsleague@gmail.com>
2019-06-16 12:44:08 -0400

    finish the clean navbar

--- src/components/header.js
+++ src/components/header.js
@@ -1,7 +1,6 @@
 import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
-import Logo from "../components/logo"
 import VSEENLogo from "../images/VSEEN-LOGO-WHITE.png"
 
 import "./header.css"
@@ -11,18 +10,16 @@ const Header = ({ siteTitle }) => (
     <div
       style={{
         background: "rgba(0, 0, 0, 0.8)",
-        paddingTop: "6px",
         textDecoration: "none",
-        position: "fixed",
         width: "100%",
         top: "0",
         zIndex: "10000",
       }}
     >
-      <Link to="/">
-        <img src={VSEENLogo} className="logo"></img>
-      </Link>
       <ul className="navbar">
+        <Link to="/" className="buttonheader">
+          <img src={VSEENLogo} className="tab logo"></img>
+        </Link>
         <li className="buttonheader">
           <Link to="/about" className="tab">
             ABOUT
--- src/components/layout.js
+++ src/components/layout.js
@@ -24,21 +24,13 @@ const Layout = ({ children }) => (
       }
     `}
     render={data => (
-      <>
+      <div>
         <Header siteTitle={data.site.siteMetadata.title} />
-        <div
-          style={{
-            margin: `0 auto`,
-            maxWidth: 960,
-            padding: `0px 1.0875rem 1.45rem`,
-            paddingTop: 0,
-          }}
-        >
+        <div>
           <main>{children}</main>
-          <footer>
-          </footer>
+          <footer></footer>
         </div>
-      </>
+      </div>
     )}
   />
 )
