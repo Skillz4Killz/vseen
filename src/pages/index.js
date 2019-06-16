5d02cfd8ee755cc4e18a60af5cd0cc0eb69b9e68
Skillz <gankstarsleague@gmail.com>
2019-06-16 12:44:45 -0400

    add the main video

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,9 +3,22 @@ import Layout from "../components/layout"
 import SEO from "../components/seo"
 
 import "../components/index.css"
+
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
+    <div className="homevid">
+      <video playsInline autoPlay loop muted className="homevid">
+        <source
+          src="https://vseen.com/wp-content/uploads/2019/02/VSEEN-Brand-Video.mp4"
+          type="video/mp4"
+        />
+        <source
+          src="https://vseen.com/wp-content/uploads/2019/02/VSEEN-Brand-Video.webm"
+          type="video/webm"
+        />
+      </video>
+    </div>
   </Layout>
 )
 
